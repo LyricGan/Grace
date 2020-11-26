e06fede87d66be84ed07eba56281187f4a7c9fcb
LyricGan <lyricgan88@163.com>
2020-11-26 23:44:24 +0800

    图片加载工具类调整；

--- library_grace/src/main/java/com/lyricgan/grace/image/ImageLoader.java
+++ library_grace/src/main/java/com/lyricgan/grace/image/ImageLoader.java
@@ -4,14 +4,9 @@ import android.content.Context;
 import android.widget.ImageView;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 
 import com.bumptech.glide.Glide;
-import com.bumptech.glide.RequestBuilder;
-import com.bumptech.glide.load.DataSource;
-import com.bumptech.glide.load.engine.GlideException;
 import com.bumptech.glide.request.RequestListener;
-import com.bumptech.glide.request.target.Target;
 
 import java.io.File;
 
@@ -37,22 +32,19 @@ public class ImageLoader {
         load(view, objUrl, placeholderId, scaleType, null);
     }
 
-    public void load(ImageView view, Object objUrl, int placeholderId, ImageView.ScaleType scaleType, ImageRequestListener<?> listener) {
+    public void load(ImageView view, Object objUrl, int placeholderId, ImageView.ScaleType scaleType, RequestListener<?> listener) {
         ImageLoaderOptions options = ImageLoaderOptions.getDefaultConfig(placeholderId).transform(scaleType);
         load(view, objUrl, options, listener);
     }
 
     public void load(ImageView view, Object objUrl, @NonNull ImageLoaderOptions options, RequestListener listener) {
-        if (view == null) {
-            return;
-        }
-        Context context = view.getContext();
-        if (objUrl == null) {
+        if (view == null || objUrl == null) {
             return;
         }
         try {
-            RequestBuilder<?> builder = Glide.with(context).asBitmap();
-            builder.load(objUrl)
+            Glide.with(view)
+                    .asBitmap()
+                    .load(objUrl)
                     .apply(options.getRequestOptions())
                     .listener(listener)
                     .into(view);
@@ -75,27 +67,5 @@ public class ImageLoader {
 
     public File getPhotoCacheDir(Context context, String cacheName) {
         return Glide.getPhotoCacheDir(context, cacheName);
-    }
-
-    /**
-     * 图片加载回调接口
-     * @param <R> 资源类型
-     */
-    public static abstract class ImageRequestListener<R> implements RequestListener<R> {
-        @Override
-        public boolean onLoadFailed(@Nullable GlideException e, Object model, Target<R> target, boolean isFirstResource) {
-            onLoadFailed(model, isFirstResource);
-            return false;
-        }
-
-        @Override
-        public boolean onResourceReady(R resource, Object model, Target<R> target, DataSource dataSource, boolean isFirstResource) {
-            onLoadSuccess(resource, model, isFirstResource);
-            return false;
-        }
-
-        public abstract void onLoadFailed(Object model, boolean isFirstResource);
-
-        public abstract void onLoadSuccess(R resource, Object model, boolean isFirstResource);
     }
 }
