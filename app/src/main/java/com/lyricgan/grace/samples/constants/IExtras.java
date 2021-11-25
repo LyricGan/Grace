8432e6d91af8a39de82248bc6ed2a42af0bdac4b
LyricGan <lyricgan88@163.com>
2021-11-25 13:02:00 +0800

    示例调整

--- app/src/main/java/com/lyricgan/grace/samples/constants/IExtras.java
+++ app/src/main/java/com/lyricgan/grace/samples/constants/IExtras.java
@@ -9,9 +9,5 @@ public interface IExtras {
     String KEY_NAME = "key_name";
     String KEY_TITLE = "key_title";
     String KEY_URL = "key_url";
-    String KEY_DATA = "key_data";
-    String KEY_RESULT = "key_result";
-    String KEY_TYPE = "key_type";
     String KEY_PARAMS = "key_params";
-    String KEY_CONTENT = "key_content";
 }
